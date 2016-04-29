dc509352eb41247e5be8fb83a795e4fd8437740f
matthiasstein <m.stein@conterra.de>
2016-04-29 08:31:24 +0200

    Add project

---
+++ app/src/main/java/stein/fbg/hsbo/de/jswebviewapp/WebViewFragment.java
@@ -0,0 +1,39 @@
+package stein.fbg.hsbo.de.jswebviewapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WebViewFragment extends Fragment {
+
+    private static String URL = "https://developers.arcgis.com/javascript/beta/sample-code/satellites-3d/live/index.html";
+
+    public WebViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+        WebView myWebView = (WebView) view.findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl(URL);
+        return view;
+    }
+
+
+}
