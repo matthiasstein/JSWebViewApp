5f0edd940e3b11ea6ef4c43a30b4915b4706b939
SebaDro <sebastian.drost@hs-bochum.de>
2016-05-11 13:59:11 +0200

    + add select button and spinner for selection mode

--- app/src/main/java/stein/fbg/hsbo/de/jswebviewapp/MainActivity.java
+++ app/src/main/java/stein/fbg/hsbo/de/jswebviewapp/MainActivity.java
@@ -14,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -50,7 +52,19 @@ public class MainActivity extends AppCompatActivity
             transaction.replace(R.id.content_frame, new WebViewFragment(), "fragment");
             transaction.commit();
         }
+        loadSpinner();
 
+    }
+
+    private void loadSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_selection);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.array_selection_mode, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
