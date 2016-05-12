140a3b113126267ba106fbf9158daee5e7afd8f7
matthiasstein <m.stein@conterra.de>
2016-05-12 16:18:42 +0200

    Change ArcGIS API for JavaScript to 3.16

--- app/src/main/assets/Map.js
+++ app/src/main/assets/Map.js
@@ -1,19 +1,14 @@
 require([
-  "esri/Map",
-  "esri/views/MapView",
+  "esri/map",
   "dojo/domReady!"
-], function(Map, MapView){
-  var map = this.map = new Map({
+], function(Map){
+  map = new Map("mapDiv", {
+    center: [-56.049, 38.485],
+    zoom: 3,
     basemap: "streets"
   });
-  var view = new MapView({
-    container: "viewDiv",  // Reference to the scene div created in step 5
-    map: map,  // Reference to the map object created before the scene
-    zoom: 4,  // Sets the zoom level based on level of detail (LOD)
-    center: [15, 65]  // Sets the center point of view in lon/lat
-  });
   function changeBasemap(basemap){
-      this.map.basemap = basemap;
+      this.map.setBasemap(basemap)
   }
   window.changeBasemap = changeBasemap;
 });
